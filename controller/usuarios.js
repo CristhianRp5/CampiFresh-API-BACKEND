dfeba6d9372bbd113dd4ac32c1aeea00b9a7a247
Cristhian Rodriguez <cristhianp252@gmial.com>
2025-02-10 13:21:26 -0500

    EntregafINAL

--- controller/usuarios.js
+++ controller/usuarios.js
@@ -216,6 +216,20 @@ class usuariosController {
   catch(error) {
     res.status(500).send( error.message);
   }
+
+  async update(req,res){
+    try {
+
+      const {id}= req.params
+      const data = await usuariosModel.update(id,req.body)
+      res.status(200).json({msg: 'Usuario actualizado', data})
+      
+    } catch (error) {
+
+      res.status(500).json(error.message)
+      
+    }
+  }
 }
 
 
